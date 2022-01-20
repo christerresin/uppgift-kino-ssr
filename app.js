13cd4adc9319cfab91877cef453f0a9c313dc2ca
Christer Resin <christer.resin@gmail.com>
2022-01-20 12:10:09 +0100

    refactor code to english and implement marked using handlebars triple {{{}}} to not escape HTML

--- app.js
+++ app.js
@@ -1,46 +1,46 @@
-import express from 'express';
-import { engine } from 'express-handlebars';
-import { marked } from 'marked';
-import DataRetriever from './scripts/DataRetriever.js';
+import express from "express";
+import { engine } from "express-handlebars";
+import { marked } from "marked";
+import DataRetriever from "./scripts/DataRetriever.js";
 
 const app = express();
 
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', './views');
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
 
 const dataLoader = new DataRetriever();
 const moviesData = await dataLoader.loadMovies();
 
 const menuItems = [
-  { label: 'Home', link: '/' },
-  { label: 'Movies', link: '/filmer' },
+  { label: "Home", link: "/" },
+  { label: "Movies", link: "/movies" },
 ];
 
-app.get('/', (req, res) => {
-  res.render('home', { menuItems: menuItems });
+app.get("/", (req, res) => {
+  res.render("home", { menuItems: menuItems });
 });
 
-app.get('/filmer', (req, res) => {
-  res.render('movies', { menuItems: menuItems, movies: moviesData });
+app.get("/movies", (req, res) => {
+  res.render("movies", { menuItems: menuItems, movies: moviesData });
 });
 
-app.get('/film/:id', async (req, res) => {
+app.get("/movie/:id", async (req, res) => {
   const movieData = await dataLoader.loadMovie(req.params.id);
   const introMarked = marked.parse(movieData.attributes.intro);
   if (movieData) {
-    res.render('movie', {
+    res.render("movie", {
       menuItems: menuItems,
       movie: movieData,
       introMarked: introMarked,
     });
   } else {
     res.status(404);
-    res.render('404');
+    res.render("404");
   }
 });
 
-app.use(express.static('./public'));
-app.use('/film', express.static('./public'));
+app.use(express.static("./public"));
+app.use("/movie", express.static("./public"));
 
 app.listen(5080);
