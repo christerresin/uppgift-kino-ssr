5c9132624b0d1fa7ee8c99bb8e09d6b98092678a
Chrikelin <Christer.resin@gmail.com>
2022-01-20 19:41:58 +0100

    add test for movie/id page

--- tests/pages.spec.js
+++ tests/pages.spec.js
@@ -8,3 +8,7 @@ test('home page loads', async () => {
 test('movies page shows list of movies', async () => {
   const response = await request(app).get('/movies').expect(200);
 });
+
+test('movie page loads', async () => {
+  const response = await request(app).get('/movie/1').expect(200);
+});
