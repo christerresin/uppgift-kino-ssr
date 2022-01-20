efe473d36689038de3ea4e03689a2a52a35ec3d5
Chrikelin <Christer.resin@gmail.com>
2022-01-20 19:40:38 +0100

    add test for home page

--- tests/pages.spec.js
+++ tests/pages.spec.js
@@ -1,6 +1,10 @@
 import request from 'supertest';
 import app from '../src/app.js';
 
+test('home page loads', async () => {
+  const response = await request(app).get('/').expect(200);
+});
+
 test('movies page shows list of movies', async () => {
   const response = await request(app).get('/movies').expect(200);
 });
