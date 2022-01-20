53f0daee45442d54e5ebfcd22d0122b48473bc2c
Chrikelin <Christer.resin@gmail.com>
2022-01-20 20:09:08 +0100

    add more scenarios to movies and movie tests

--- tests/pages.spec.js
+++ tests/pages.spec.js
@@ -7,8 +7,13 @@ test('home page loads', async () => {
 
 test('movies page shows list of movies', async () => {
   const response = await request(app).get('/movies').expect(200);
+
+  expect(response.text.includes('Threat')).toBeTruthy();
+  expect(response.text.includes('Idiocracy')).toBeTruthy();
 });
 
-test('movie page loads', async () => {
-  const response = await request(app).get('/movie/1').expect(200);
+test('movie page loads and has movie title', async () => {
+  const response = await request(app).get('/movie/10').expect(200);
+
+  expect(response.text.includes('Threat')).toBeTruthy();
 });
