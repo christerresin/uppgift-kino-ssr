01f61f708df382e9a28e6aece3443aad871ec29a
Christer Resin <christer.resin@gmail.com>
2022-01-21 10:39:56 +0100

    add test for movie intro and imdb link

--- tests/pages.spec.js
+++ tests/pages.spec.js
@@ -1,32 +1,32 @@
-import { response } from 'express';
-import request from 'supertest';
-import app from '../src/app.js';
+import { response } from "express";
+import request from "supertest";
+import app from "../src/app.js";
 
-test('home page loads', async () => {
-  const response = await request(app).get('/').expect(200);
+test("home page loads", async () => {
+  const response = await request(app).get("/").expect(200);
 });
 
-test('movies page shows list of movies', async () => {
-  const response = await request(app).get('/movies').expect(200);
+test("movies page shows list of movies", async () => {
+  const response = await request(app).get("/movies").expect(200);
 
-  expect(response.text.includes('Threat')).toBeTruthy();
-  expect(response.text.includes('Idiocracy')).toBeTruthy();
-  expect(response.text.includes('Knight')).toBeTruthy();
-  expect(response.text.includes('Godfather')).toBeTruthy();
+  expect(response.text.includes("Threat")).toBeTruthy();
+  expect(response.text.includes("Idiocracy")).toBeTruthy();
+  expect(response.text.includes("Knight")).toBeTruthy();
+  expect(response.text.includes("Godfather")).toBeTruthy();
 });
 
-test('movie page loads, has movie title and image', async () => {
+test("movie page loads, has movie title and image", async () => {
   const response = await request(app)
-    .get('/movie/10')
+    .get("/movie/10")
     .expect(200)
     .then((response) => {
       console.log(response);
       return response;
     });
-
-  expect(response.text.includes('Threat')).toBeTruthy();
-  const testKeyWords = ['background-image', 'media-amazon', '/images/', '.jpg'];
+  expect(response.text.includes("Threat")).toBeTruthy();
+  const testKeyWords = ["background-image", "media-amazon", "/images/", ".jpg"];
   expect(
     testKeyWords.every((word) => response.text.includes(word))
   ).toBeTruthy();
+  expect(response.text.includes("imdb")).toBeTruthy();
 });
